b9a813b29ed249dd73897020a11bafb832d532bc
Muzhi Yang <yangmuzhi1993@gmail.com>
2017-11-12 16:23:37 -0800

    some ui change

--- DotProjectOne/Controllers/Item4Controller.cs
+++ DotProjectOne/Controllers/Item4Controller.cs
@@ -6,12 +6,11 @@ using System.Web.Mvc;
 
 namespace DotProjectOne.Controllers
 {
-    public class Item1Controller : Controller
+    public class Item4Controller : Controller
     {
         public ActionResult Index()
         {
-            ViewData["Title"] = "Item1";
-            return View ("~/Views/Item/item1.cshtml");
+            return View ("~/Views/Item/item4.cshtml");
         }
     }
 }
--- DotProjectOne/Controllers/Item5Controller.cs
+++ DotProjectOne/Controllers/Item5Controller.cs
@@ -6,12 +6,11 @@ using System.Web.Mvc;
 
 namespace DotProjectOne.Controllers
 {
-    public class Item1Controller : Controller
+    public class Item5Controller : Controller
     {
         public ActionResult Index()
         {
-            ViewData["Title"] = "Item1";
-            return View ("~/Views/Item/item1.cshtml");
+            return View ("~/Views/Item/item5.cshtml");
         }
     }
 }
